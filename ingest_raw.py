a5dd02f4740a2aaada90ec237b3b455e7c881440
Ginko <sengoku500@gmail.com>
2026-05-06 20:01:13 +0700

    update project

--- ingest_raw.py
+++ ingest_raw.py
@@ -2,9 +2,14 @@ import pandas as pd
 import sqlalchemy as sa
 from dotenv import load_dotenv
 import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
+
 db_url = os.getenv("SUPABASE_DB_URL")
+if not db_url:
+    raise ValueError("SUPABASE_DB_URL not set in env")
 
 engine = sa.create_engine(db_url,echo=False)
 
@@ -12,16 +17,13 @@ print("Creating schema 'raw'...")
 with engine.connect() as conn:
     conn.execute(sa.text("CREATE SCHEMA IF NOT EXISTS raw;"))
     conn.commit()
-print("Schema 'raw' created (or already exists)")
 
 url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
-print(f"Downloading from: {url}")
+logging.info(f"Downloaded {url} rows")
 
 df = pd.read_parquet(url)
+logging.info(f"Downloaded {len(df):,} rows")
 
-print(f"Downloaded {len(df):,} rows")
-
-print("Loading to database...")
 df.to_sql(
     name="taxi_trips",
     con=engine,
@@ -31,4 +33,4 @@ df.to_sql(
     chunksize=50000
 )
 
-print("Raw data loaded successfully into raw.taxi_trips")
+logging.info("Raw data loaded successfully into raw.taxi_trips")
